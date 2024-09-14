12335aa78ba4303f528024a026141339c5bf60a8
alexcip999 <58331044+alexcip999@users.noreply.github.com>
2024-09-14 15:25:07 +0300

    MessageCard commit
    in this commit I added the structure of a Message Card which is compossed from an image, a title medium text and a textbox which ilustrate the content of the message.

--- app/src/main/java/com/example/conversation_app/MainActivity.kt
+++ app/src/main/java/com/example/conversation_app/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
@@ -13,12 +15,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,17 +45,76 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(message: Message){
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Profile Picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        val isExpanded by remember {
+            mutableStateOf(false)
+        }
+        val surfaceColor by animateColorAsState(
+            targetValue = if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            label = ""
+        )
+
+        Column {
+            Text(
+                text = message.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.width(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = message.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard(){
+    MessageCard(Message("Alexandru Constantin", "Cf, iesi?"))
+}
+
+data class Acount (val username: String, val activity: String)
+
 @Preview(showBackground = true)
 @Composable
 fun TopAppConversationBar(){
     //var onClick: () -> Unit = {}
+    val user = Acount("Username", "activity")
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(all = 32.dp),
         verticalAlignment = Alignment.CenterVertically
     ){
-        FilledButton { }
+        BackButton { }
 
         Spacer(modifier = Modifier.width(8.dp))
 
@@ -62,23 +129,21 @@ fun TopAppConversationBar(){
 
         Spacer(modifier = Modifier.width(8.dp))
         
-        // a class which get username and details
         Column {
             Text(
-                text = "Username"
+                text = user.username
             )
             Text(
-                text = "cv"
+                text = user.activity
             )
         }
-        //////////////////////////////////////////
 
     }
 }
 
 
 @Composable
-fun FilledButton(onClick: () -> Unit){
+fun BackButton(onClick: () -> Unit){
     Button(
         onClick = {
             onClick()
@@ -91,3 +156,22 @@ fun FilledButton(onClick: () -> Unit){
         )
     }
 }
+
+@Composable
+fun Conversation(messages: List<Message>){
+    LazyColumn {
+        items(messages){
+            message -> MessageCard(message)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
